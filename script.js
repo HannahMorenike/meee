b0a5ed10243a47a783b332cfaa58e52d321425e4
Hannah <40662847+HannahMorenike@users.noreply.github.com>
2024-10-03 11:03:46 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+//turn pages when click next or prev button
+//contact me button when click
+//create reverse index function
+//back profile button when click
+//opening animation
+//opening animation (cover right animation)
+//opening animation (page left or profile page animation)
+//opening animation (all page right animation)
+
+const resumeLists = document.querySelectorAll('.');
+const resumeLists = document.querySelectorAll('.resume-list');
